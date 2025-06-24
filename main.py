048c736042370b27f61eb655bf7ab112b4e38b08
Dariusmumbere <dariusmumbere@gmail.com>
2025-06-24 10:10:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends, status
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional, Dict, Any
 from datetime import datetime, timezone
 import asyncpg
