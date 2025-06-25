33fa9e92d65b400d536db83e3aa6cfc0b9eb382c
Dariusmumbere <dariusmumbere@gmail.com>
2025-06-25 13:21:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app = FastAPI(
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://dariusmumbere.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
